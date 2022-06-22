46ea63d986e701fc42f83f27a86ac7d4ebd706fe
Adisa Barakat <67762572+Adekemi02@users.noreply.github.com>
2022-06-22 16:44:11 +0100

    Add files via upload

---
+++ myPage.js
@@ -0,0 +1,11 @@
+alert("Welcome")
+let user = prompt("Enter your name")
+console.log(`Here is your printed name: ${user}`)
+
+let height = prompt("What's your height?")
+console.log (`Your height: ${height}`)
+
+let country = prompt("Enter the name of your country")
+console.log(`Your country is : ${country}`)
+
+alert("You have to the end of the session")
